c8e469baf55f7a0c345d496417ecc9deaec46df8
Fortunate-Panda <0xfortunatepanda@proton.me>
2026-04-23 16:49:36 +0100

    Update axios baseURL to new endpoint

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://52.53.181.83:8080',
+    baseURL:'http://a09c2265ea8e14299b091a9f3976a558-625612663.us-west-1.elb.amazonaws.com:8080',
     headers: {
         'Content-Type': 'application/json',
     },
